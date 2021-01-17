d03345715039ec3485e4da9efc9992095aae1b14
Mandarinka450 <nastya-s-01@mail.ru>
2021-01-17 16:56:26 +0300

    add video and finish project

--- PycharmProjects/pythonProject/venv/Scripts/activate.bat
+++ PycharmProjects/pythonProject/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Admin\PycharmProjects\pythonProject\venv
+set VIRTUAL_ENV=C:\Users\Admin\engineering_proj-3sem\PycharmProjects\pythonProject\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
